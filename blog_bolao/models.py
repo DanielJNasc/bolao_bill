98f07d25821ca9677abaa8955e0d07991eebee5b
Daniel <danieljorgeln@gmail.com>
2022-08-27 16:43:42 -0300

    - Add field fase to jogo - Alter field estadio on jogo mudança no admin para registar o jogo criação da fase do campeonatoh

--- blog_bolao/models.py
+++ blog_bolao/models.py
@@ -7,10 +7,19 @@ from django.utils import timezone
 
 
 class Jogo(models.Model):
+    TIPO_DE_FASE =(
+        ('grupos', 'Fase de Grupos'),
+        ('oitavas', 'Oitavas de final'),
+        ('quartas', 'Quartas de final'),
+        ('semi', 'Semi Final'),
+        ('final', 'Final'),
+    )
     criador = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_1 = models.CharField(max_length=200)
     time_2 = models.CharField(max_length=200)
-    estadio = models.TextField()
+    fase = models.CharField(max_length=30, choices=TIPO_DE_FASE, blank=True)
+    resultado = models.CharField(max_length=200, default='Esperando o jogo')
+    estadio = models.CharField(max_length=100)
     data_de_criacao = models.DateTimeField(default=timezone.now)
     data_do_jogo = models.DateTimeField(blank=True, null=True)
 
