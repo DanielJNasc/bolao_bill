e7911efc1930b1f9d320e755f31342638f278557
Daniel <danieljorgeln@gmail.com>
2022-08-27 18:18:55 -0300

    criação da classe Time registyro do time no site

--- blog_bolao/views.py
+++ blog_bolao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Jogo
 
 # Create your views here.
+def jogo_list(request):
+    todos_os_jogos = Jogo.objects.all()
+    return render(request, 'blog_bolao/jogo_list.html', {'jogos': todos_os_jogos})
+
