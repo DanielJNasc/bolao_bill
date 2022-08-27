ec6279ee0d2a328057b5647129cad050883e9df7
Daniel <danieljorgeln@gmail.com>
2022-08-27 17:08:14 -0300

    - Add field fase to jogo - Create model Estadio - Alter field estadio on jogo criamos a migração

--- blog_bolao/models.py
+++ blog_bolao/models.py
@@ -5,6 +5,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Estadio(models.Model):
+    nome = models.CharField(max_length=100)
+    capacidade = models.IntegerField()
+    cidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} - {self.cidade}'
 
 class Jogo(models.Model):
     TIPO_DE_FASE =(
@@ -19,7 +26,7 @@ class Jogo(models.Model):
     time_2 = models.CharField(max_length=200)
     fase = models.CharField(max_length=30, choices=TIPO_DE_FASE, blank=True)
     resultado = models.CharField(max_length=200, default='Esperando o jogo')
-    estadio = models.CharField(max_length=100)
+    estadio = models.ForeignKey('Estadio', on_delete=models.CASCADE)
     data_de_criacao = models.DateTimeField(default=timezone.now)
     data_do_jogo = models.DateTimeField(blank=True, null=True)
 
